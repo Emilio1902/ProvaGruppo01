5e68a7843d10aac835812ebbbdd8c732071f34be
clari <clari@host.docker.internal>
2021-11-16 18:24:17 +0100

    Cose a caso

---
+++ mavenproject1/src/main/java/it/unisa/mavenproject1/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.mavenproject1;
+
+/**
+ *
+ * @author clari
+ */
+public class NewClass {
+    public int add(int a, int b) {
+        return a+b;
+    }
+    
+    public int dot(int a, int b) {
+        return a*b;
+    }
+}
---
+++ mavenproject1/src/test/java/NewClassTest.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import it.unisa.mavenproject1.NewClass;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+/**
+ *
+ * @author clari
+ */
+public class NewClassTest {
+    private NewClass nc;
+    
+    @Test
+    public void testAdd(){
+        assertEquals(7, nc.add(3, 4));
+        assertEquals(5, nc.add(10, -5));
+        
+    }
+    
+    public void testDot(){
+       
+        assertEquals(7, nc.dot(3, 4));
+        assertEquals(5, nc.dot(10, -5));
+        
+    }
+}
