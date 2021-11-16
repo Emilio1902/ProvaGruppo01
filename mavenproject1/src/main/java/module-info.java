a53ce8e51935da918f28b0025bea493d1846c0be
clari <clari@host.docker.internal>
2021-11-16 17:37:17 +0100

    Aggiunta stampa Ciao

---
+++ mavenproject1/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module it.unisa.mavenproject1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens it.unisa.mavenproject1 to javafx.fxml;
+    exports it.unisa.mavenproject1;
+}
+*/
