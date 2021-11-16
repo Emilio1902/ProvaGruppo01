c90954e3e4aab8266dbdd3ec68b7aae05c35996a
raffa <raffa@LAPTOP-NND9FO0D.mshome.net>
2021-11-16 17:41:16 +0100

    Stampa Emilio

--- mavenproject1/src/main/java/it/unisa/mavenproject1/PrimaryController.java
+++ mavenproject1/src/main/java/it/unisa/mavenproject1/PrimaryController.java
@@ -9,5 +9,6 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
         System.out.println("Ciao");
+        System.out.println("Emilio");
     }
 }
